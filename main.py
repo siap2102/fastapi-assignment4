16ee58ab8d81de993bdd06979de9bfb8bd2264de
Sia Padole <siapadole3@gmail.com>
2025-07-08 00:04:04 +0530

    feat: add PUT endpoint to update item

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+yfrom fastapi import FastAPI
 from typing import Optional
 
 app = FastAPI()
@@ -10,4 +10,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "query": q}
+
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
+    available: bool
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "updated_item": item}
 
